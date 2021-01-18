65598cb928c3e26d8770ce7dd806dabff5423fc4
openamiguel <mopena@princeton.edu>
2021-01-18 11:23:42 -0700

    Changed uint to uint[N] types

--- src/contracts/Marketplace.sol
+++ src/contracts/Marketplace.sol
@@ -3,23 +3,23 @@ pragma solidity ^0.5.0;
 contract Marketplace {
 
 	bytes32 public name;
-	uint public productCount;
-	uint public maxProductsPerBuyerPerIssuer = 2; 
-	uint public percentFee = 3; // MUST NEVER BE ZERO!!!
+	uint64 public productCount;
+	uint8 public maxProductsPerBuyerPerIssuer = 2; 
+	uint8 public percentFee = 3; // MUST NEVER BE ZERO!!!
 
 	address payable owner; 
 
-	mapping(uint => Product) public products;
-	mapping(address => mapping(address => uint)) public productsPerBuyerPerIssuer; 
+	mapping(uint64 => Product) public products;
+	mapping(address => mapping(address => uint32)) public productsPerBuyerPerIssuer; 
 
 	// Note on uint: uint256 by default, aiiowing 2**256-1 ~ 1.16e77 unique tickets
 	struct Product {
 		address payable issuer; // Issuer of ticket (e.g., event host, trusted third party)
 		address payable holder; // Holder of ticket
 		bytes32 name; // Location of ticket in venue
-		uint id; // Unique ticket ID
-		uint price; // Price in Ether? Wei?
-		uint percentRefund; // Non-refundable products (percentRefund == 0) can be purchased but not returned
+		uint64 id; // Unique ticket ID
+		uint128 price; // Price in Wei
+		uint8 percentRefund; // Non-refundable products (percentRefund == 0) can be purchased but not returned
 		bool purchased; // Purchased products cannot be bought again
 		bool returnRequested; // Buyer can request refunds, but seller must unilaterally approve requests
 		bool withdrawn; // Withdrawn products can be returned but not purchased
@@ -29,21 +29,21 @@ contract Marketplace {
 		address payable issuer,
 		address payable holder,
 		bytes32 name,
-		uint id,
-		uint price,
-		uint percentRefund, 
+		uint64 id,
+		uint128 price,
+		uint8 percentRefund, 
 		bool purchased, 
 		bool returnRequested, 
 		bool withdrawn
 	);
 
-	event ProductWithdrawn(uint id);
+	event ProductWithdrawn(uint64 id);
 
-	event ReturnRequested(uint id);
+	event ReturnRequested(uint64 id);
 
-	event ProductPurchased(uint id);
+	event ProductPurchased(uint64 id);
 
-	event ProductReturned(uint id);
+	event ProductReturned(uint64 id);
 
 	constructor() public {
 		name = "Ticketly";
@@ -56,7 +56,7 @@ contract Marketplace {
 		_; 
 	}
 
-	modifier validProductID(uint _id) {
+	modifier validProductID(uint64 _id) {
 		// Check if product has valid ID
 		require(_id > 0 && _id <= productCount, "Invalid product ID passed to this function.");
 		_; 
@@ -69,7 +69,7 @@ contract Marketplace {
 
 	// msg.sender is the issuer
 	// Transaction fee for owner: 0 Wei
-	function createProduct(string calldata _name, uint _price, uint _percentRefund) external {
+	function createProduct(string calldata _name, uint64 _price, uint8 _percentRefund) external {
 		uint len = bytes(_name).length; 
 		// Require a valid name
 		require(len > 0, "Invalid name passed to product creation function."); 
@@ -82,7 +82,7 @@ contract Marketplace {
 		// Increment product count
 		productCount++;
 		// Create product
-		products[productCount] = Product(productCount, _name, _price, msg.sender, msg.sender, false, _percentRefund, false, false);
+		products[productCount] = Product(msg.sender, msg.sender, _name, productCount, _price, _percentRefund, false, false, false);
 		// Trigger an event
 		emit ProductCreated(msg.sender, msg.sender, _name, productCount, _price, _percentRefund, false, false, false);
 	}
@@ -91,9 +91,9 @@ contract Marketplace {
 		address payable issuer,
 		address payable holder,
 		string name,
-		uint id,
-		uint price,
-		uint percentRefund, 
+		uint64 id,
+		uint128 price,
+		uint8 percentRefund, 
 		bool purchased, 
 		bool returnRequested, 
 		bool withdrawn
@@ -101,7 +101,7 @@ contract Marketplace {
 
 	// msg.sender is the issuer
 	// Irreversibly withdraws products[_id] from the market
-	function withdrawProduct(uint _id) validProductID(_id) external {
+	function withdrawProduct(uint64 _id) validProductID(_id) external {
 		// Fetch the product
 		Product memory _product = products[_id];
 		// Require that issuer is valid
@@ -116,7 +116,7 @@ contract Marketplace {
 
 	// msg.sender is the buyer
 	// Requests for issuer to refund item
-	function requestReturn(uint _id) validProductID(_id) external {
+	function requestReturn(uint64 _id) validProductID(_id) external {
 		// Fetch the product
 		Product memory _product = products[_id];
 		// Require that product is refundable
@@ -135,7 +135,7 @@ contract Marketplace {
 
 	// msg.sender is the buyer
 	// Transaction fee for owner: percentFee pct of ticket price
-	function purchaseProduct(uint _id) validProductID(_id) external payable {
+	function purchaseProduct(uint64 _id) validProductID(_id) external payable {
 		// Fetch the product
 		Product memory _product = products[_id];
 		// Fetch the issuer
@@ -168,7 +168,7 @@ contract Marketplace {
 
 	// msg.sender is the issuer
 	// Transaction fee for owner: percentFee pct of ticket price
-	function returnProduct(uint _id) validProductID(_id) external payable {
+	function returnProduct(uint64 _id) validProductID(_id) external payable {
 		// Fetch the product
 		Product memory _product = products[_id];
 		// Fetch the holder
